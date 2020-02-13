15e5544e981bda3e4ecace3990e4b952668ba171
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-13 21:52:50 +0545

    corrected the java file

--- Advance_Java_Course/src/advance_java_course/Advance_Java_Course.java
+++ Advance_Java_Course/src/advance_java_course/Advance_Java_Course.java
@@ -19,15 +19,25 @@ public class Advance_Java_Course {
      */
     public static void main(String[] args) {
         // This java program finds the next perfect square number that is provided by the 
-        Assignments obj = new Assignments();
-        Scanner input = new Scanner(System.in);
+//        Assignments obj = new Assignments();
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a number to find its next perfect square");
+//        int num = input.nextInt();
+//        System.out.println("The next perfect number is :" + obj.next_Perfect_Square(num));
 
-        System.out.println("Enter a number to find its next perfect square");
-
-        int num = input.nextInt();
-
-        System.out.println("The next perfect number is :" + obj.next_Perfect_Square(num));
-
+        
+//        Java Program to count the prime number starting from the start number 
+//        to the end number given by the user
+        
+        Assignments primecounter = new Assignments();
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter the start number :");
+        int start =s.nextInt();
+        
+        System.out.println("Enter the end number :");
+        int end = s.nextInt();
+        System.out.println("The number of prime number between "+start+" and "+end+" is "+primecounter.primeCount(start, end));
     }
 
 }
