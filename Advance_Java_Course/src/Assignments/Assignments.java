d14ea2eb093345acbce0ef37380411839176b697
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-13 22:15:24 +0545

    added comment

--- Advance_Java_Course/src/Assignments/Assignments.java
+++ Advance_Java_Course/src/Assignments/Assignments.java
@@ -12,7 +12,7 @@ package Assignments;
 public class Assignments {
 
     int num;
-
+//Function for calculating the next perfect number 
     public double next_Perfect_Square(int n) {
         this.num = n + 1;
         double var = Math.ceil(Math.sqrt(num));
@@ -22,17 +22,21 @@ public class Assignments {
         }
         return num;
     }
-    
+    //Program for counting the prime number between the two number provided by the user
     public int primeCount(int start,int end){
         int count=0;
         for(int i=start;i<=end;i++){
             int var=2;
             if(i%var==0){
                 var++;
+                
             }
             else{
                 count++;
+                
+                System.out.println(i);
             }
+
         }
         return count;
     }
