230fb40b3778f1f9c421b7659c3d4d57b5cf768f
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-21 16:22:39 +0545

    Question_3

--- Advance_Java_Course/src/Assignments/Question_3.java
+++ Advance_Java_Course/src/Assignments/Question_3.java
@@ -10,6 +10,41 @@ package Assignments;
  * @author Dell
  */
 public class Question_3 {
+
+    
+    public void isInertial(int[] a){
+        for (int i = 0; i < a.length; i++) {
+            
+            if (a[i]%2 == 0) {
+                int[] evenarr;
+                System.out.println("the even number is:"+a[i]);
+            } 
+            else if(a[i]%2!=0){
+                int[] oddarr;
+                System.out.println("The odd number is :"+a[i]);
+                
+            }
+            else {
+                
+                    
+                    
+            }
+            
+        }
+        
+        
+
+    }
     
     
+    
+    
+
+    public static void main(String[] args) {
+        int[] a = {2, 4, 5, 7, 8};
+        
+       Question_3 q = new Question_3();
+       q.isInertial(a);
+    }
+
 }
