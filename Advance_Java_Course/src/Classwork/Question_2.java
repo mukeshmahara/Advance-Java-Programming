2f802ae10ff240d6e61615050522ca4963b2c129
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-21 11:38:59 +0545

    fibonaci number detecter

---
+++ Advance_Java_Course/src/Classwork/Question_2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classwork;
+
+/**
+ *
+ * @author Dell
+ */
+public class Question_2 {
+    
+    Question_2(){
+        
+    }
+    
+    
+    public static void main(String[] args){
+       int a = 123;//this is the fibonaci number 
+       
+       int var=10; 
+       
+       while(a/var<1){
+           
+           a = a/var;
+           int[] arr={};
+           
+           System.out.println("value of var:"+a);
+       }
+       
+        
+       
+    }
+    
+}
