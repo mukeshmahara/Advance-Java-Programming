df9050004404eb4f5c5d97f11d6c9eee5bb66e17
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-21 11:38:33 +0545

    swap method impl

---
+++ Advance_Java_Course/src/Classwork/Question_1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classwork;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dell
+ */
+//Write a java program to swap two int variables. without using third or temp variable
+
+public class Question_1 {
+    public static void main(String[] args){
+//        List<Integer> num = new ArrayList<>();
+//        num.add(1);
+//        num.add(2);
+//        System.out.println("Before Sswap :"+num);
+//        Collections.swap(num, 0, 1);
+//        System.out.println("After Swap :"+num
+//        );
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter the value of a :");
+        int input=s.nextInt();
+        System.out.println(""+input);
+        
+    }
+}
