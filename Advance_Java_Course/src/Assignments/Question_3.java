a8cabc24e4a8a30dfcee2cae76d93283df0b5adf
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-21 20:40:35 +0545

    Question_3

--- Advance_Java_Course/src/Assignments/Question_3.java
+++ Advance_Java_Course/src/Assignments/Question_3.java
@@ -11,40 +11,28 @@ package Assignments;
  */
 public class Question_3 {
 
-    
-    public void isInertial(int[] a){
+    public void isInertial(int[] a) {
         for (int i = 0; i < a.length; i++) {
-            
-            if (a[i]%2 == 0) {
-                int[] evenarr;
-                System.out.println("the even number is:"+a[i]);
-            } 
-            else if(a[i]%2!=0){
-                int[] oddarr;
-                System.out.println("The odd number is :"+a[i]);
-                
+            for (int j = 0; j < a.length; j++) {
+//                int b = a[j];
+
+                if (a[i] % 2 == 0) {
+                    if (a[j] < a[i]) {
+                        a[1]=a[i];   
+                    }
+                }
             }
-            else {
-                
-                    
-                    
-            }
-            
         }
-        
-        
+        System.out.println("The greatest even number is:"+a[1]);
+             
 
     }
-    
-    
-    
-    
-
     public static void main(String[] args) {
-        int[] a = {2, 4, 5, 7, 8};
         
-       Question_3 q = new Question_3();
-       q.isInertial(a);
+        int[] a = {2, 4, 8, 20, 23, 27, 30};
+
+        Question_3 q = new Question_3();
+        q.isInertial(a);
     }
 
 }
