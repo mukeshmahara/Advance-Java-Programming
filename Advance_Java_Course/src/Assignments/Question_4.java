1d05b6e9019cdee1f822f91448ea8142ac45e8e4
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-23 07:22:33 +0545

    countSquarePair()

--- Advance_Java_Course/src/Assignments/Question_4.java
+++ Advance_Java_Course/src/Assignments/Question_4.java
@@ -10,49 +10,53 @@ package Assignments;
  * @author Dell
  */
 public class Question_4 {
+
     private int count;
 
-	Question_4() {
-		count = 0;
-	}
+    Question_4() {
+        count = 0;
+    }
 
-	public int countSquarePair(int[] array){
-		
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array.length; j++) {
-				if(isSquarePair(array[i],array[j]))
-					count++;
-				
-			}
-			
-		}
-		return count;
-		
-	}
+    public int countSquarePair(int[] array) {
 
-	public boolean isSquarePair(int a, int b) {
-		if (a <= 0 || b <= 0 || a >= b)
-			return false;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (isSquarePair(array[i], array[j])) {
+                    count++;
+                }
 
-		if (isPerfectSquare(a + b))
-			return true;
-		else
-			return false;
-	}
+            }
 
-	public boolean isPerfectSquare(int a) {
-		int temp = (int) Math.sqrt(a);
-		if (temp * temp == a) {
-			return true;
-		} else
-			return false;
-	}
-        
-        
-        public static void main(String[] args){
-            Question_4 _4 =new Question_4();
-            int[] a={11, 5, 4, 20} ;
-		System.out.println(_4.countSquarePair(a));
         }
-    
+        return count;
+
+    }
+
+    public boolean isSquarePair(int a, int b) {
+        if (a <= 0 || b <= 0 || a >= b) {
+            return false;
+        }
+
+        if (isPerfectSquare(a + b)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isPerfectSquare(int a) {
+        int temp = (int) Math.sqrt(a);
+        if (temp * temp == a) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Question_4 _4 = new Question_4();
+        int[] a = {11, 5, 4, 20, 16, 36};
+        System.out.println(_4.countSquarePair(a));
+    }
+
 }
