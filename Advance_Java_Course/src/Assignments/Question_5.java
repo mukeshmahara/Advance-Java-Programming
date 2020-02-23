051a50a9793cebaa16516ae5d51c215794d28578
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-23 07:28:27 +0545

    porcupine number

--- Advance_Java_Course/src/Assignments/Question_5.java
+++ Advance_Java_Course/src/Assignments/Question_5.java
@@ -1,10 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
-Question:
-'''''''''
-5.   A prime number is an integer that is divisible only by 1 and itself. A porcupine number is a prime number whose last digit is 9 and the next prime number that follows it also ends with the digit 9. For example 139 is a porcupine number because:
+ 
+ Question:
+ '''''''''
+ 5.   A prime number is an integer that is divisible only by 1 and itself. A porcupine number is a prime number whose last digit is 9 and the next prime number that follows it also ends with the digit 9. For example 139 is a porcupine number because:
  a. it is prime
  b. it ends in a 9
  c. The next prime number after it is 149 which also ends in 9. Note that 140, 141, 142, 143, 144, 145, 146, 147 and 148 are not prime so 149 is the next prime number after 139.
@@ -24,56 +22,58 @@ package Assignments;
  * @author Dell
  */
 public class Question_5 {
+
     public int findPorcupineNumber(int n) {
-    int num = n + 1;
-		int lastDigit;
-		int primeNum,newPrime = 0;
-		while (true) {
-			// find the prime number which is greater than the given number
+        int num = n + 1;
+        int lastDigit;
+        int primeNum, newPrime = 0;
+        while (true) {
+            // find the prime number which is greater than the given number
 
-			primeNum = findPrimeNumber(num);
-			newPrime=primeNum;
-			// prime number found .Now lets check the last digit of the prime
-			// Number which we have found
-			lastDigit = primeNum % 10;
-			if (lastDigit == 9) {
-				// find leading prime number
-				newPrime = findPrimeNumber(primeNum);
-				lastDigit = newPrime % 10;
-				// check the last digit
-				if (lastDigit == 9) {
-					// finally porcupine number is found
-					return primeNum;
+            primeNum = findPrimeNumber(num);
+            newPrime = primeNum;
+            // prime number found .Now lets check the last digit of the prime
+            // Number which we have found
+            lastDigit = primeNum % 10;
+            if (lastDigit == 9) {
+                // find leading prime number
+                newPrime = findPrimeNumber(primeNum);
+                lastDigit = newPrime % 10;
+                // check the last digit
+                if (lastDigit == 9) {
+                    // finally porcupine number is found
+                    return primeNum;
 
-				}
-			}
-			num = newPrime;
+                }
+            }
+            num = newPrime;
 
-		}
-	}
-
-	private int findPrimeNumber(int num) {
-		int i = num + 1;
-		int j;
-		while (true) {
-
-			for (j = 2; j < i; j++) {
-				if (i % j == 0)
-					break;
-
-			}
-				if (j == i) {
-					return i;
-				}
-			
-			i++;
-		}
-	}
-        
-        public static void main(String[] args){
-            
-            Question_5 _5 = new Question_5();
-		System.out.println(_5.findPorcupineNumber(0));
         }
+    }
+
+    private int findPrimeNumber(int num) {
+        int i = num + 1;
+        int j;
+        while (true) {
+
+            for (j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    break;
+                }
+
+            }
+            if (j == i) {
+                return i;
+            }
+
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Question_5 _5 = new Question_5();
+        System.out.println(_5.findPorcupineNumber(0));
+    }
 
 }
