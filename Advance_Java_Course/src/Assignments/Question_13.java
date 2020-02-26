dd0e10feb2e5a103eb8b47cd44a5e223e955fa3b
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-26 23:17:06 +0545

    isSquare(int n)

--- Advance_Java_Course/src/Assignments/Question_13.java
+++ Advance_Java_Course/src/Assignments/Question_13.java
@@ -14,23 +14,36 @@ import java.util.Scanner;
 public class Question_13 {
 
     int isSquare(int n) {
-        int lastNum = n%10;
-        if (lastNum ==0 || lastNum ==1 || lastNum ==4 || lastNum ==5 || lastNum ==6 || lastNum ==9) {
+        int lastNum = n % 10;
+//        if (lastNum ==0 || lastNum ==1 || lastNum ==4 || lastNum ==5 || lastNum ==6 || lastNum ==9) {
+//            
+//            return 1;
+//            
+//        } else  {
+//            return 0;
+//        }
+        int inc = 0;
+        do {
+            inc++;
+            if (inc>n) {
+                
+                System.out.println("not not a square!!");
+                return 0;
+            }
             
-            return 1;
-            
-        } else  {
-            return 0;
-        }
-
+        } while (inc * inc != n);
+        
+     return 1;
     }
 
+    
+    
     public static void main(String[] args) {
         Question_13 q = new Question_13();
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number");
         int input = s.nextInt();
 
-        System.out.println("Retrun :"+q.isSquare(input));
+        System.out.println("Return :" + q.isSquare(input));
     }
 }
