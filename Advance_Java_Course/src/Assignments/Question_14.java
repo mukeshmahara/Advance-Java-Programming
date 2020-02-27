db5e7b1e10649b7fa2d32b010b9b8175e50062ae
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-27 18:58:23 +0545

    Stacked Number

--- Advance_Java_Course/src/Assignments/Question_14.java
+++ Advance_Java_Course/src/Assignments/Question_14.java
@@ -5,10 +5,43 @@
  */
 package Assignments;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Dell
  */
 public class Question_14 {
-    
+
+    public int isStacked(int num) {
+        int sum = 0;
+
+        do {
+            sum += sum;
+            sum++;
+            if (sum > num) {
+                System.out.println("impossible number");
+                break;
+            }
+
+        } while (sum < num);
+
+        if (sum == num) {
+            System.out.println("The number is stacked number...");
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Question_14 q = new Question_14();
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a number to check whether it is stacked number or not:");
+        int input = s.nextInt();
+
+        System.out.println("Return :" + q.isStacked(input));
+    }
+
 }
