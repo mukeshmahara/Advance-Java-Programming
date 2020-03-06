8f6cf79cbe00d2ea344eda7f1e5ffbfec9d9df36
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-03-06 21:16:48 +0545

    DBConfig

--- Advance_Java_Course/src/DB_Access/DB_config.java
+++ Advance_Java_Course/src/DB_Access/DB_config.java
@@ -16,17 +16,17 @@ public class DB_config {
 
     public static void main(String args[]) {
         try {
-//step1 load the driver class  
+        //step1 load the driver class  
             Class.forName("oracle.jdbc.driver.OracleDriver");
 
-//step2 create  the connection object  
+        //step2 create  the connection object  
             Connection con = DriverManager.getConnection(
                     "jdbc:oracle:thin:@localhost:1521:orcl", "aj", "aj");
 
-//step3 create the statement object  
+        //step3 create the statement object  
             Statement stmt = con.createStatement();
 
-//step4 execute query  
+        //step4 execute query  
             ResultSet rs = stmt.executeQuery("select * from countries");
             System.out.println("Country_id\t"+"Country_name\t\t"+"Region ID");
             while (rs.next()) {
@@ -34,7 +34,7 @@ public class DB_config {
                 System.out.println( rs.getString(1) + "\t\t" + rs.getString(2)+""+ rs.getInt(3));
             }
 
-//step5 close the connection object  
+        //step5 close the connection object  
             con.close();
 
         } catch (Exception e) {
