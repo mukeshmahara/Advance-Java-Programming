2b319639189ecde7ce446c15038dabeb0f738b0f
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-04-04 22:53:26 +0545

    Registration Servlet

--- MeroApp/src/java/MeroApp/com/OnlineShopingApplication/Register_Servlet.java
+++ MeroApp/src/java/MeroApp/com/OnlineShopingApplication/Register_Servlet.java
@@ -17,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -58,7 +59,8 @@ public class Register_Servlet extends HttpServlet {
 
             Statement stmt = con.createStatement();
             
-
+            HttpSession merosessoion = request.getSession();
+            
             out.println("</body>");
             out.println("</html>");
         } catch (SQLException ex) {
